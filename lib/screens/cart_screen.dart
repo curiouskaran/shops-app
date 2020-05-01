0a2d91c1d26fef21c3d19ab4fab31a3208e8467d
karan.s1 <karan.s1@naukri.com>
2020-05-01 06:36:18 +0530

    added orders provider, add order and clear cart functinality

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../providers/cart.dart' show Cart;
 import '../widgets/cart_item.dart';
+import '../providers/cart.dart' show Cart;
+import '../providers/orders.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -40,7 +41,16 @@ class CartScreen extends StatelessWidget {
                           backgroundColor: Theme.of(context).primaryColor,
                         ),
                         FlatButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              Provider.of<Orders>(
+                                context,
+                                listen: false,
+                              ).addOrder(
+                                cart.items.values.toList(),
+                                cart.totalAmount,
+                              );
+                              cart.clear();
+                            },
                             child: Text(
                               'ORDER NOW',
                               style: TextStyle(
